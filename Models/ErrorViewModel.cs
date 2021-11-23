d4b120f474b167ad9af1e3c77f60b89230310857
fan9704 <cxz123499@gmail.com>
2021-11-23 19:44:11 +0800

    git init and create Product Controller And View

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace shopping_mall.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
